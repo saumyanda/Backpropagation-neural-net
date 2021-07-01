b303f424dcf306ec5af3197ef08135027c587d3e
Saumya Shandilya <saumyanda@gmail.com>
2021-07-01 13:36:48 -0700

    Create Action.java

---
+++ LUT_Robot/Action.java
@@ -0,0 +1,14 @@
+package ece.cpen502;
+public class Action {
+
+	public static final int NumberOfRobotActions = 6;
+	public static final double RobotMoveDistance = 300.0;
+	public static final double RobotTurningDegree =  20.0;
+	public static final int RobotAhead = 0;
+	public static final int RobotBack = 1;
+	public static final int RobotAheadTurnLeft = 2;
+	public static final int RobotAheadTurnRight = 3;
+	public static final int RobotBackTurnLeft = 4;
+	public static final int RobotBackTurnRight = 5;
+
+}
